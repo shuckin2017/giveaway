5282fea23ef0334c5f4498dfee9a2f41819fc62a
Maxim Schuckin <shuckin@mail.ru>
2021-11-11 13:03:00 +0300

    minor update

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,35 @@
 const sliderThumbs = new Swiper('.slider__thumbs .swiper-container', {
-    // ищем слайдер превью по селектору
-    // задаем параметры
-    direction: 'vertical', // вертикальная прокрутка
-    slidesPerView: 3, // показывать по 3 превью
-    spaceBetween: 24, // расстояние между слайдами
+    direction: 'vertical',
+    slidesPerView: 3,
+    spaceBetween: 24,
     navigation: {
-        // задаем кнопки навигации
-        nextEl: '.slider__next', // кнопка Next
-        prevEl: '.slider__prev', // кнопка Prev
+        nextEl: '.slider__next',
+        prevEl: '.slider__prev',
     },
-    freeMode: true, // при перетаскивании превью ведет себя как при скролле
+    freeMode: true,
     breakpoints: {
-        // условия для разных размеров окна браузера
         0: {
-            // при 0px и выше
-            direction: 'horizontal', // горизонтальная прокрутка
-        },
-        768: {
-            // при 768px и выше
-            direction: 'vertical', // вертикальная прокрутка
+            direction: 'vertical',
         },
     },
 });
-// Инициализация слайдера изображений
+
 const sliderImages = new Swiper('.slider__images .swiper-container', {
-    // ищем слайдер превью по селектору
-    // задаем параметры
-    direction: 'vertical', // вертикальная прокрутка
-    slidesPerView: 1, // показывать по 1 изображению
-    spaceBetween: 32, // расстояние между слайдами
-    mousewheel: true, // можно прокручивать изображения колёсиком мыши
+    direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 32,
+    mousewheel: true,
     navigation: {
-        // задаем кнопки навигации
-        nextEl: '.slider__next', // кнопка Next
-        prevEl: '.slider__prev', // кнопка Prev
+        nextEl: '.slider__next',
+        prevEl: '.slider__prev',
     },
-    grabCursor: true, // менять иконку курсора
+    grabCursor: true,
     thumbs: {
-        // указываем на превью слайдер
-        swiper: sliderThumbs, // указываем имя превью слайдера
+        swiper: sliderThumbs,
     },
     breakpoints: {
-        // условия для разных размеров окна браузера
         0: {
-            // при 0px и выше
-            direction: 'horizontal', // горизонтальная прокрутка
-        },
-        768: {
-            // при 768px и выше
-            direction: 'vertical', // вертикальная прокрутка
+            direction: 'vertical',
         },
     },
 });
